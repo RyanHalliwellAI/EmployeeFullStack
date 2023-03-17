d10f08af58afcd6a0d187cabbe29209e91b5f413
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-03-17 17:50:07 +1030

    Adding Delete request for sql

--- EmployeeFullStack/Controllers/DepartmentController.cs
+++ EmployeeFullStack/Controllers/DepartmentController.cs
@@ -120,6 +120,41 @@ namespace EmployeeFullStack.Controllers
 
             return Ok(result);
         }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.Department 
+                            where DepartmentId = @DepartmentId";
 
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+
+                    myCommand.Parameters.AddWithValue("@DepartmentId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+            // Convert DataTable to List of Dictionaries
+            var result = table.AsEnumerable()
+                .Select(row => table.Columns.Cast<DataColumn>()
+                .ToDictionary(col => col.ColumnName, col => row[col]))
+                .ToList();
+
+            return Ok(result);
+        }
     }
+
 }
+
