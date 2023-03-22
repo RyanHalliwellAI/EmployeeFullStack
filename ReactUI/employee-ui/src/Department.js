307e0e62062665c63ff92a1be0aebd7b98c30aaf
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-03-22 18:20:48 +1030

    Adding employee features

--- ReactUI/employee-ui/src/Department.js
+++ ReactUI/employee-ui/src/Department.js
@@ -88,6 +88,28 @@ export class Department extends Component{
             alert('Failed');
         })
     }
+    deleteClick(id){
+        console.log(id);
+        console.log(variables.API_URL);
+
+        if(window.confirm('Are you sure?')){
+        fetch(variables.API_URL+'department/'+id,{
+            method:'DELETE',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            }
+        })
+        .then(res=>res.json())
+        
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+        }
+    }
 
 
     
@@ -125,7 +147,9 @@ export class Department extends Component{
                             <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                             </svg>
                         </button>
-                        <button type = "button" className="btn btn-light mr-1">
+                        <button type = "button" 
+                        className="btn btn-light mr-1"
+                        onClick = {() => this.deleteClick(dep.DepartmentId)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                     </svg>
